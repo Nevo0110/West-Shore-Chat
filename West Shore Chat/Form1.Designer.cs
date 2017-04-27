16e19993b59a925a9256470c03b6773a964eabe7
Nicholas Gordon <ngordon@westshore.edu>
2017-04-27 18:23:05 -0400

    Memes

--- West Shore Chat/Form1.Designer.cs
+++ West Shore Chat/Form1.Designer.cs
@@ -30,9 +30,14 @@
         {
             this.mainText = new System.Windows.Forms.RichTextBox();
             this.messageText = new System.Windows.Forms.RichTextBox();
+            this.connectionStatusLabel = new System.Windows.Forms.Label();
             this.connectionStatus = new System.Windows.Forms.Label();
-            this.connectionStatus1 = new System.Windows.Forms.Label();
             this.send = new System.Windows.Forms.Button();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.Label();
+            this.locationLabel = new System.Windows.Forms.Label();
+            this.location = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainText
@@ -44,7 +49,7 @@
             this.mainText.Name = "mainText";
             this.mainText.ReadOnly = true;
             this.mainText.Size = new System.Drawing.Size(707, 358);
-            this.mainText.TabIndex = 0;
+            this.mainText.TabIndex = 1;
             this.mainText.Text = "";
             // 
             // messageText
@@ -52,29 +57,33 @@
             this.messageText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.messageText.Location = new System.Drawing.Point(13, 377);
+            this.messageText.Multiline = false;
             this.messageText.Name = "messageText";
             this.messageText.Size = new System.Drawing.Size(707, 32);
-            this.messageText.TabIndex = 1;
+            this.messageText.TabIndex = 0;
             this.messageText.Text = "";
+            this.messageText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.messageText_KeyDown);
+            // 
+            // connectionStatusLabel
+            // 
+            this.connectionStatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectionStatusLabel.AutoSize = true;
+            this.connectionStatusLabel.Location = new System.Drawing.Point(731, 13);
+            this.connectionStatusLabel.Name = "connectionStatusLabel";
+            this.connectionStatusLabel.Size = new System.Drawing.Size(97, 13);
+            this.connectionStatusLabel.TabIndex = 2;
+            this.connectionStatusLabel.Text = "Connection Status:";
             // 
             // connectionStatus
             // 
+            this.connectionStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.connectionStatus.AutoSize = true;
-            this.connectionStatus.Location = new System.Drawing.Point(731, 13);
+            this.connectionStatus.ForeColor = System.Drawing.Color.Red;
+            this.connectionStatus.Location = new System.Drawing.Point(731, 26);
             this.connectionStatus.Name = "connectionStatus";
-            this.connectionStatus.Size = new System.Drawing.Size(97, 13);
-            this.connectionStatus.TabIndex = 2;
-            this.connectionStatus.Text = "Connection Status:";
-            // 
-            // connectionStatus1
-            // 
-            this.connectionStatus1.AutoSize = true;
-            this.connectionStatus1.ForeColor = System.Drawing.Color.Red;
-            this.connectionStatus1.Location = new System.Drawing.Point(731, 26);
-            this.connectionStatus1.Name = "connectionStatus1";
-            this.connectionStatus1.Size = new System.Drawing.Size(79, 13);
-            this.connectionStatus1.TabIndex = 3;
-            this.connectionStatus1.Text = "Not Connected";
+            this.connectionStatus.Size = new System.Drawing.Size(79, 13);
+            this.connectionStatus.TabIndex = 3;
+            this.connectionStatus.Text = "Not Connected";
             // 
             // send
             // 
@@ -87,19 +96,75 @@
             this.send.UseVisualStyleBackColor = true;
             this.send.Click += new System.EventHandler(this.send_Click);
             // 
+            // userLabel
+            // 
+            this.userLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(731, 53);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(78, 13);
+            this.userLabel.TabIndex = 5;
+            this.userLabel.Text = "Connected To:";
+            // 
+            // user
+            // 
+            this.user.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.user.AutoSize = true;
+            this.user.Location = new System.Drawing.Point(731, 66);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(29, 13);
+            this.user.TabIndex = 6;
+            this.user.Text = "User";
+            // 
+            // locationLabel
+            // 
+            this.locationLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.locationLabel.AutoSize = true;
+            this.locationLabel.Location = new System.Drawing.Point(731, 100);
+            this.locationLabel.Name = "locationLabel";
+            this.locationLabel.Size = new System.Drawing.Size(99, 13);
+            this.locationLabel.TabIndex = 7;
+            this.locationLabel.Text = "Computer Location:";
+            // 
+            // location
+            // 
+            this.location.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.location.AutoSize = true;
+            this.location.Location = new System.Drawing.Point(731, 113);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(34, 13);
+            this.location.TabIndex = 8;
+            this.location.Text = "Blank";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(748, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.location);
+            this.Controls.Add(this.locationLabel);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.userLabel);
             this.Controls.Add(this.send);
-            this.Controls.Add(this.connectionStatus1);
             this.Controls.Add(this.connectionStatus);
+            this.Controls.Add(this.connectionStatusLabel);
             this.Controls.Add(this.messageText);
             this.Controls.Add(this.mainText);
             this.MinimumSize = new System.Drawing.Size(860, 460);
             this.Name = "mainForm";
-            this.Text = "Help Desk";
+            this.ShowIcon = false;
+            this.Text = "Help Desk Chat";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +174,14 @@
 
         private System.Windows.Forms.RichTextBox mainText;
         private System.Windows.Forms.RichTextBox messageText;
+        private System.Windows.Forms.Label connectionStatusLabel;
         private System.Windows.Forms.Label connectionStatus;
-        private System.Windows.Forms.Label connectionStatus1;
         private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label user;
+        private System.Windows.Forms.Label locationLabel;
+        private System.Windows.Forms.Label location;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- West Shore Chat/Form1.cs
+++ West Shore Chat/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace West_Shore_Chat
 {
@@ -26,9 +27,37 @@ namespace West_Shore_Chat
 
         }
 
+        private void saveLog()
+        {
+            DateTime DT = DateTime.Now;
+
+            string test = "Hey there beter " + Convert.ToString(DT) + ".txt";
+            MessageBox.Show(test);
+            /*DateTime currentTime = DateTime.Now;
+
+            FileStream fs = new FileStream(@"HelpDeskLog " + Convert.ToString(currentTime) + ".txt", FileMode.CreateNew, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine(mainText);
+            sw.Close(); */
+        }
+
         private void send_Click(object sender, EventArgs e)
         {
             sendMessage();
+        }
+
+        private void messageText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                sendMessage();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveLog();
         }
     }
 }
